c214df96f8e651baba767a2a4de3a1b2b20e9bb3
Irwin Arruda <arruda.irwin@gmail.com>
2024-01-07 19:12:03 -0300

    Adds new vscode keybindings and settings.

--- nvim/lua/app/plugins/nvim-treesitter.lua
+++ nvim/lua/app/plugins/nvim-treesitter.lua
@@ -1,37 +1,44 @@
 local s1, nvimtreesitter = pcall(require, "nvim-treesitter.configs")
 
 if not s1 then
-	return
+  return
 end
 
 nvimtreesitter.setup({
-	ensure_installed = {
-		"c",
-		"lua",
-		"vim",
-		"vimdoc",
-		"html",
-		"typescript",
-		"javascript",
-		"tsx",
-		"svelte",
-		"vue",
-		"java",
-		"json",
-		"xml",
-	},
-	sync_install = false,
-	autoinstall = true,
-	autotag = {
-		enable = true,
-		enable_rename = true,
-		enable_close = true,
-		enable_close_on_slash = true,
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
+  ensure_installed = {
+    "c",
+    "lua",
+    "vim",
+    "vimdoc",
+    "html",
+    "typescript",
+    "javascript",
+    "tsx",
+    "svelte",
+    "vue",
+    "java",
+    "json",
+    "xml",
+  },
+  sync_install = false,
+  autoinstall = true,
+  autotag = {
+    enable = true,
+    enable_rename = true,
+    enable_close = true,
+    enable_close_on_slash = true,
+  },
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 })
 
 require("nvim-ts-autotag").setup()
+
+-- on windows
+-- local s2, nvimtreesitterinstall = pcall(require, "nvim-treesitter.install")
+-- if not s2 then
+--   return
+-- end
+-- nvimtreesitterinstall.compilers = { "clang" }
