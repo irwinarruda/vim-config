de3f7693e5cb3034cd893ded567c862a05b14a53
irwinarruda <arruda.irwin@gmail.com>
2024-01-22 21:50:08 -0300

    fixes tsserver lsp config

--- nvim/lua/app/plugins/lsp.lua
+++ nvim/lua/app/plugins/lsp.lua
@@ -53,30 +53,6 @@ end
 
 lsp.on_attach(on_attach)
 
-require("typescript-tools").setup({
-  on_attach = on_attach,
-  handlers = { lsp.default_setup },
-  settings = {
-    separate_diagnostic_server = true,
-    publish_diagnostic_on = "insert_leave",
-    expose_as_code_action = {},
-    tsserver_path = nil,
-    tsserver_plugins = {},
-    tsserver_max_memory = "auto",
-    tsserver_format_options = {},
-    tsserver_file_preferences = {},
-    tsserver_locale = "en",
-    complete_function_calls = false,
-    include_completions_with_insert_text = true,
-    code_lens = "off",
-    disable_member_code_lens = true,
-    jsx_close_tag = {
-      enable = false,
-      filetypes = { "javascriptreact", "typescriptreact" },
-    },
-  },
-})
-
 require("mason").setup({})
 require("mason-lspconfig").setup({
   ensure_installed = {
@@ -91,7 +67,31 @@ require("mason-lspconfig").setup({
   },
   handlers = {
     lsp.default_setup,
-    tsserver = lsp.noop(),
+    tsserver = function()
+      require("typescript-tools").setup({
+        on_attach = on_attach,
+        handlers = { lsp.default_setup },
+        settings = {
+          separate_diagnostic_server = true,
+          publish_diagnostic_on = "insert_leave",
+          expose_as_code_action = {},
+          tsserver_path = nil,
+          tsserver_plugins = {},
+          tsserver_max_memory = "auto",
+          tsserver_format_options = {},
+          tsserver_file_preferences = {},
+          tsserver_locale = "en",
+          complete_function_calls = false,
+          include_completions_with_insert_text = true,
+          code_lens = "off",
+          disable_member_code_lens = true,
+          jsx_close_tag = {
+            enable = false,
+            filetypes = { "javascriptreact", "typescriptreact" },
+          },
+        },
+      })
+    end,
     jsonls = function()
       require("lspconfig").jsonls.setup({
         on_attach = on_attach,
