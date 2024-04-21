c081d557b696af8e775a36700a9839b2be75f9ca
Irwin Arruda <arruda.irwin@gmail.com>
2024-04-21 19:58:52 -0300

    feat: add nvim-spectre

---
+++ nvim/lua/app/plugins/nvim-spectre.lua
@@ -0,0 +1,12 @@
+return {
+  "nvim-pack/nvim-spectre",
+  event = "VeryLazy",
+  config = function()
+    local spectre = require('spectre')
+    local keymap = vim.keymap
+    keymap.set("n", "<leader>ss", spectre.toggle)
+    keymap.set("n", "<leader>sf", function()
+      spectre.open_visual({ select_word = true })
+    end)
+  end,
+}
