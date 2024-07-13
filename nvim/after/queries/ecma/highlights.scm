4e527817b10da17720dfc42b824d654e9a1dcfb3
irwinarruda <arruda.irwin@gmail.com>
2024-07-13 01:26:52 -0300

    chore: update after file

--- nvim/after/queries/ecma/highlights.scm
+++ nvim/after/queries/ecma/highlights.scm
@@ -7,6 +7,6 @@
 
 ((import_clause
   (named_imports
-    (import_specifier) @variable (set! "priority" 105))))
+    (import_specifier) @variable (#set! "priority" 105))))
 (import_clause
-  (identifier) @variable (set! "priority" 105))
+  (identifier) @variable (#set! "priority" 105))
