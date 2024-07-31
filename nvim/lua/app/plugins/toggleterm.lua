be1ca63423f2175d3a2b73ba70b391e49654c851
irwinarruda <arruda.irwin@gmail.com>
2024-07-31 00:18:30 -0300

    feat: adds powershell for windows

--- nvim/lua/app/plugins/toggleterm.lua
+++ nvim/lua/app/plugins/toggleterm.lua
@@ -2,6 +2,17 @@ local function is_terminals_open()
   return require("toggleterm.ui").find_open_windows()
 end
 
+local is_windows = require("nvim-os-persist").is_windows
+if is_windows then
+  vim.opt.shell = "powershell"
+  vim.opt.shellcmdflag =
+    "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+  vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+  vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+  vim.opt.shellquote = ""
+  vim.opt.shellxquote = ""
+end
+
 return {
   "akinsho/toggleterm.nvim",
   event = "VeryLazy",
