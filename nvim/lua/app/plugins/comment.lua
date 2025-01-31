6bece5b9e72d6ef9fdc7cdfc586d859304efde2a
irwinarruda <arruda.irwin@gmail.com>
2025-01-31 08:53:12 -0300

    feat: adds comments to jsx

--- nvim/lua/app/plugins/comment.lua
+++ nvim/lua/app/plugins/comment.lua
@@ -1,10 +1,15 @@
 return {
+
   "numToStr/Comment.nvim",
   event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+  },
   config = function()
     local comment = require("Comment")
     local os = require("app.plugins.nvim-os-persist")
     comment.setup({
+      pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
       post_hook = function(ctx)
         local U = require("Comment.utils")
         if ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
